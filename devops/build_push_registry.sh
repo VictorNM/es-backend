0abda317271976404596d6d3e49617826d29fd8c
Phuc Le <bobgel12@gmail.com>
2020-04-17 08:30:46 -0700

    add scripts

---
+++ devops/build_push_registry.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# This script push image to docker registry
+
+set -o errexit
+
+echo "---Install IBM Cloud CLI and registry plugins"
+curl -fsSL https://clis.cloud.ibm.com/install/linux | sh
+
+echo "---Login into IBM Cloud CLI"
+ibmcloud api cloud.ibm.com
+ibmcloud login --apikey $IBMCLOUD_API_KEY -c $IBMCLOUD_ACC_ID
+ibmcloud cr login
+
+echo "---Build Docker Image"
+docker build . -f ../Dockerfile -t esbackend:$TRAVIS_BUILD_NUMBER
+
+echo "---Tag docker image with IBM Cloud"
+docker tag esbackend:$TRAVIS_BUILD_NUMBER us.icr.io/esregistry/esbackend:$TRAVIS_BUILD_NUMBER
+
+echo "---Push docker image with IBM Cloud Registry"
+docker push us.icr.io/esregistry/esbackend:$TRAVIS_BUILD_NUMBER
---
+++ devops/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# This script deploy to kubernetes
+
+NAMESPACE="es-dev" # or "es-test" or "es-prod"
+
+echo "Deploying to $NAMESPACE namespace"
+
+echo "Install KUBECTL"
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt` $TRAVIS_BUILD_DIR/kubectl
+chmod +x $TRAVIS_BUILD_DIR/kubectl
+export KUBECTL = $TRAVIS_BUILD_DIR/kubectl
+
+echo "Applying yaml files"
+$KUBECTL --namespace $NAMESPACE apply -f $TRAVIS_BUILD_DIR/devops/yaml/back-end-deployment.yaml
+$KUBECTL --namespace $NAMESPACE apply -f $TRAVIS_BUILD_DIR/devops/yaml/back-end-service.yaml
