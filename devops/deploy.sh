9502fe6ae7b4db100b6846c1badd2660fc8aa8e2
Phuc Le <bobgel12@gmail.com>
2020-04-17 09:41:16 -0700

    Update deploy.sh

--- devops/deploy.sh
+++ devops/deploy.sh
@@ -6,9 +6,9 @@ NAMESPACE="es-dev" # or "es-test" or "es-prod"
 echo "Deploying to $NAMESPACE namespace"
 
 echo "Install KUBECTL"
-curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt` $TRAVIS_BUILD_DIR/kubectl
+curl -sSL "https://storage.googleapis.com/kubernetes-release/release/v1.14.9/bin/linux/amd64/kubectl" > $TRAVIS_BUILD_DIR/kubectl
 chmod +x $TRAVIS_BUILD_DIR/kubectl
-export KUBECTL = $TRAVIS_BUILD_DIR/kubectl
+export KUBECTL=$TRAVIS_BUILD_DIR/kubectl
 
 echo "Applying yaml files"
 $KUBECTL --namespace $NAMESPACE apply -f $TRAVIS_BUILD_DIR/devops/yaml_temp/back-end-deployment.yaml
